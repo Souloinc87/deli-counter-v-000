6db5c2a6f49350eeb6cb78771c84a8e057472bf4
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 23:44:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   elsif  array.length >= 1
-    array.collect {|person| puts "Currently serving #{person}."}
+    array.each {|person| puts "Currently serving #{person}."}
   end
 end
