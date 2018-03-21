ca77e57dd59e4ff862e700306141ee3b99201910
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 22:42:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   elsif array.length >= 1
-    puts "The line is currently: 1. #{array[0]} + 2. #{array[1]} 3. #{array[2]}"
+    puts "The line is currently: 1. #{array[0]}  2. #{array[1]} 3. #{array[2]}"
   end
 end
