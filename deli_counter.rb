2d1994f9dc5b2c71a2e8ac9a018c9d90329e2eca
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 22:51:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(array)
     puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
   elsif array.length >= 8
     puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]} 4. #{array[3]} 5. #{array[4]}
-  6. #{array[5]} 7. #{array[6]} 8. #{array[7]} 9. #{array[8]} 10. #{array[9]}"
+6. #{array[5]} 7. #{array[6]} 8. #{array[7]} 9. #{array[8]} 10. #{array[9]}"
   end
 end
