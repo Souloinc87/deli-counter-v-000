085e74013523fad038b4ef629eae6271dfcce246
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 23:34:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,11 @@ def take_a_number(array, name)
    array.push(name)
    puts "Welcome, #{name}. You are number #{array.length} in line."   
 end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!" 
+  elsif  array.length >= 1
+    array.map {|person| return "Currently serving #{person}."}
+  end 
+end 
